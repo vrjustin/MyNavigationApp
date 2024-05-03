c22a02a844c723639e98555e068f129d04564029
Justin Maronde <myemailisjustin@me.com>
2024-05-03 07:10:21 -0400

    install and implement stack navigator example

---
+++ src/screens/Screen03Details.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Screen03Details = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Screen03 Details</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Screen03Details;
