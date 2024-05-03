5fd004814780fb04d905560dd2ff66343e6900d8
VRJustin <myemailisjustin@me.com>
2024-05-03 07:20:47 -0400

    install and implement example of a native stack navigator. (#3)

--- src/navigators/DrawerNavigator.tsx
+++ src/navigators/DrawerNavigator.tsx
@@ -3,6 +3,7 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Screen01 from '../screens/Screen01';
 import Screen02 from '../screens/Screen02';
 import StackNavigator from './StackNavigator';
+import NativeStackNavigator from './NativeStackNavigator';
 
 const Drawer = createDrawerNavigator();
 
@@ -12,6 +13,7 @@ const DrawerNavigator = () => {
       <Drawer.Screen name="Screen01" component={Screen01} />
       <Drawer.Screen name="Screen02" component={Screen02} />
       <Drawer.Screen name="ExampleStack" component={StackNavigator} />
+      <Drawer.Screen name="NativeStack" component={NativeStackNavigator} />
     </Drawer.Navigator>
   );
 };
---
+++ src/screens/Screen04.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const Screen04 = () => {
+  const navigation = useNavigation();
+  const handleNavigateToDetails = () => {
+    navigation.navigate('Screen04Details');
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Hello Screen04</Text>
+      <TouchableOpacity onPress={handleNavigateToDetails}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Go to Details</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    margin: 16,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    padding: 16,
+  },
+});
+
+export default Screen04;
